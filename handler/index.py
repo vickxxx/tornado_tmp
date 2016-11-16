831e077d392c60c6d8c29f0b59a26811bcbd12b1
vickxxx <lianghuiming@live.com>
2016-11-16 15:59:39 +0800

    错别字

--- handler/index.py
+++ handler/index.py
@@ -9,6 +9,6 @@ import tornado.web
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        welcome_word = "welcome you. this is randered by jinja2"
+        welcome_word = "welcome you. this is rendered by jinja2"
         self.render('index.html', sth=welcome_word)
 
