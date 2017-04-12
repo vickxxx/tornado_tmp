07591477b77a254173bb85d0d06ac6ccebd40efa
vickxxx <lianghuiming@live.com>
2017-04-12 14:39:17 +0800

    增加参数

--- server.py
+++ server.py
@@ -35,7 +35,7 @@ def main():
     tornado.options.parse_command_line()
     if not sys.platform.startswith('win'):
         import coloredlogs
-        coloredlogs.install(level='DEBUG', logging.getLogger("tornado"))    
+        coloredlogs.install(level='DEBUG', logger = logging.getLogger("tornado"))    
     
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
